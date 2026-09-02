93f449a91a60ed1eb0d3b6659b41863647330cf1
scriscuoli-aic <scriscuoli@gmail.com>
2026-09-02 17:06:06 -0400

    numpy time

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 from flask import Flask, render_template, jsonify
 from datetime import datetime
-from datetime import timedelta
+from datetime import date
+
+import numpy as np
 
 app = Flask(__name__)
 
 
 def get_retirement():
-    now = datetime.now()
-    year = now.year
     retirement = datetime(2027, 2, 26)
     return retirement
 
 
 def get_countdown():
     now = datetime.now()
-    christmas = get_retirement()
-    delta = christmas - now
+    retirement = get_retirement()
+    delta = retirement - now
+
+    t = date.today().isoformat()  # e.g. '2026-09-02'
+    weekdays = np.busday_count(t, '2027-02-26')
 
     days = delta.days
     hours, remainder = divmod(delta.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
 
     return {
-        "days": days,
-        "hours": hours,
-        "minutes": minutes,
-        "seconds": seconds,
+        "days": str(days),
+        "weekdays": str(weekdays),
+        "hours": str(hours),
+        "minutes": str(minutes),
+        "seconds": str(seconds)
     }
 
 @app.route("/")
